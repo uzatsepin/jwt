c7bb48bbe5eff412afe8af628b7618e4dc310553
Andrii Zatsepin <uzatsepin@gmail.com>
2023-08-03 13:27:59 +0300

    setup and setting project

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import { initializeApp } from 'firebase/app'
+
+import App from './App.vue'
+import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_mtOBnKdoCOgEOJjeDsu0DF80jZ8az4w',
+  authDomain: 'jwt-firebase-vue.firebaseapp.com',
+  projectId: 'jwt-firebase-vue',
+  storageBucket: 'jwt-firebase-vue.appspot.com',
+  messagingSenderId: '135078178271',
+  appId: '1:135078178271:web:1d9ff431227c583c32686a'
+}
+
+initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
